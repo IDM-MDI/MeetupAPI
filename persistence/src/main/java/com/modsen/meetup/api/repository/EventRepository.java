64e7718c1eb487154995a97174e41b63ee711c1d
Dayanch_Ishangulyev_Linux <ishankulievdayantch@yandex.ru>
2022-11-02 01:09:46 +0300

    add hibernate config

--- persistence/src/main/java/com/modsen/meetup/api/repository/EventRepository.java
+++ persistence/src/main/java/com/modsen/meetup/api/repository/EventRepository.java
@@ -1,4 +1,8 @@
 package com.modsen.meetup.api.repository;
 
+import com.modsen.meetup.api.dto.EventDto;
+import com.modsen.meetup.api.entity.Event;
+
 public interface EventRepository {
+    Event findByID(long id);
 }
--- persistence/src/main/java/com/modsen/meetup/api/repository/impl/BaseEventRepository.java
+++ persistence/src/main/java/com/modsen/meetup/api/repository/impl/BaseEventRepository.java
@@ -1,10 +1,25 @@
 package com.modsen.meetup.api.repository.impl;
 
+import com.modsen.meetup.api.entity.Event;
 import com.modsen.meetup.api.repository.EventRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-
+@Repository
 public class BaseEventRepository implements EventRepository {
-    private EntityManager entityManager;
 
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BaseEventRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+    @Override
+    public Event findByID(long id) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        return session.find(Event.class, id);
+    }
 }
