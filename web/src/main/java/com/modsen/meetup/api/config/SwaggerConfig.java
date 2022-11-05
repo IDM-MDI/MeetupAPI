bb3e08d3ccebdc6765b5a39b520ef8120ecba99d
Dayanch_Ishangulyev_Linux <ishankulievdayantch@yandex.ru>
2022-11-06 01:50:11 +0300

    add swagger ui

---
+++ web/src/main/java/com/modsen/meetup/api/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.modsen.meetup.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+
+@Configuration
+public class SwaggerConfig {
+    public static final String EVENT_TAG = "Event API";
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(basePackage("com.modsen.meetup.api.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .tags(new Tag(EVENT_TAG, "Event API controller with CRUD operation"));
+    }
+}
--- web/src/main/java/com/modsen/meetup/api/controller/EventController.java
+++ web/src/main/java/com/modsen/meetup/api/controller/EventController.java
@@ -1,16 +1,20 @@
 package com.modsen.meetup.api.controller;
 
+import com.modsen.meetup.api.config.SwaggerConfig;
 import com.modsen.meetup.api.dto.EventDto;
 import com.modsen.meetup.api.dto.PaginationInfo;
 import com.modsen.meetup.api.dto.ResponsePage;
 import com.modsen.meetup.api.exception.PersistenceException;
 import com.modsen.meetup.api.exception.ServiceException;
 import com.modsen.meetup.api.service.PageEventService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/event")
+@Api(tags = {SwaggerConfig.EVENT_TAG})
 public class EventController {
     private final PageEventService service;
 
@@ -19,7 +23,8 @@ public class EventController {
         this.service = service;
     }
 
-    @GetMapping()
+    @GetMapping
+    @ApiOperation("Get event list by using pagination")
     public ResponsePage<EventDto> getActiveEvents(
             @RequestParam(defaultValue = "5") long size,
             @RequestParam(defaultValue = "0") long page,
@@ -39,21 +44,25 @@ public class EventController {
     }
 
     @PostMapping
+    @ApiOperation("Create event by data from object")
     public ResponsePage<EventDto> addEvent(@RequestBody EventDto event) throws ServiceException, PersistenceException {
         return service.save(event);
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Get single event by ID from path")
     public ResponsePage<EventDto> getEventByID(@PathVariable long id) throws ServiceException {
         return service.findByID(id);
     }
     @PatchMapping("/{id}")
+    @ApiOperation("Update database event by ID to data from the body")
     public ResponsePage<EventDto> updateEvent(@PathVariable long id,
                                               @RequestBody EventDto event) throws ServiceException, PersistenceException {
         return service.update(event,id);
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation("Delete event by ID from path")
     public ResponsePage<EventDto> deleteEvent(@PathVariable long id) throws ServiceException, PersistenceException {
         return service.delete(id);
     }
